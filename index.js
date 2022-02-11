0a5b81d97a42ed4f6650a555e66360f3f1e7b1e8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 13:45:47 +0100

    add funcions on javascript

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+function imprimeHola() {
+    console.log("hola")
+}
+
+
+
+
 function main() {
     console.log("starting..");
+    const title = document.querySelector("h1");
+    title.addEventListener("pointerenter",imprimeHola);
+    const elements = Array.from(document.body.querySelectorAll("*"));
+    for (let items of elements){
+        items.addEventListener("pointerenter", imprimeHola)
+    }
 }
 window.addEventListener("load",main);
+
